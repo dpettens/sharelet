7be950943e89f292776622cf65543492de380204
Denis Pettens <denis.pettens@gmail.com>
2017-11-24 14:39:22 +0100

    [rpi] Remove unused files

--- rpi/device_control/lightON.py
+++
@@ -1,4 +0,0 @@
-import RPi.GPIO as GPIO ## Import GPIO library
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,True) ## Turn off GPIO pin 7
