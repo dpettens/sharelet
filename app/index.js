2cfb223588da5c743ae2d4cf9e14c3f5b5f6b357
Nicolas Surleraux <nsurleraux@gmail.com>
2017-12-17 10:50:28 +0100

    Fix crash if outlet is disconnected

--- app/index.js
+++ app/index.js
@@ -64,6 +64,7 @@ let wsClients = {};
 
 wsFromClients.on('connection', ws => {
   ws.on('message', message => {
+    console.log(message);
     try {
       const outlet = JSON.parse(message);
 
@@ -75,6 +76,7 @@ wsFromClients.on('connection', ws => {
       if(hash === outlet.pwd) {
         if(!wsClients.hasOwnProperty(outlet.outlet_id) || wsClients[outlet.outlet_id] != ws) {
           wsClients[outlet.outlet_id] = ws;
+          console.log("Add outlet", outlet.outlet_id);
 
           client.execute(GET_SETTINGS_START + outlet.outlet_id + GET_SETTINGS_END).then(res => {
             if(res.rows.length !== 0)
@@ -90,7 +92,7 @@ wsFromClients.on('connection', ws => {
             ws.send(JSON.stringify({res : "OK"}));
           });
 
-          let value = outlet.data[i].value;
+          let value = Math.abs(outlet.data[i].value);
           let alerts = wsClients[outlet.outlet_id].alerts;
 
           if(alerts && (value < alerts.low || value > alerts.high)) {
@@ -129,7 +131,7 @@ wsFromAPI.on('connection', ws => {
       if(true) {
         const connection = wsClients[data.target];
 
-        if(typeof connection !== 'undefined') {
+        if(typeof connection !== 'undefined' && connection.readyState === ws.OPEN) {
           connection.send(JSON.stringify({type : 0, close : data.power}));
           client.execute(UPDATE_STATE_BEGIN + data.power + UPDATE_STATE_MIDDLE + data.target + UPDATE_STATE_END, error => {
             if(error) {
@@ -137,9 +139,10 @@ wsFromAPI.on('connection', ws => {
               process.exit(-1);
             }
           });
-
+          console.log("commande ok");
           ws.send(JSON.stringify({"type" : 4, "ok" : true, "target" : data.target}));
         } else {
+          console.log("commande pas ok");
           ws.send(JSON.stringify({"type" : 4, "ok" : false, "target" : data.target}));
         }
       }
