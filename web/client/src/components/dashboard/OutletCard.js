11633f0c3eb83db884f8ed8d6dc356232d4b7261
Nicolas Surleraux <nsurleraux@gmail.com>
2017-12-18 20:54:52 +0100

    add graphs

--- web/client/src/components/dashboard/OutletCard.js
+++ web/client/src/components/dashboard/OutletCard.js
@@ -3,9 +3,37 @@ import PropTypes from 'prop-types';
 import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
 import Switch from 'material-ui/Switch';
 import Tooltip from 'material-ui/Tooltip';
+import RC2 from 'react-chartjs2';
 
 import Delete from './Delete';
 import Update from './Update';
+
+const data = {
+  labels: ['20h30', '', '20h35', '', '20h40', '', '20h45', '', '20h50', '', '20h55', '', '21h00'],
+  datasets: [
+    {
+      label: 'Consommation',
+      fill: false,
+      lineTension: 0.2,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40, 96, 87, 28, 74, 12, 54, 27],
+    }
+  ]
+};
 
 const OutletCard = ({ alias, handleChangeStateOutlet, handleDeleteOutlet, handleUpdateOutlet, id, state }) => (
   <Card>
@@ -23,6 +51,7 @@ const OutletCard = ({ alias, handleChangeStateOutlet, handleDeleteOutlet, handle
       subheader={id}
     />
     <CardContent>
+      <RC2 data={data} type='line' />
     </CardContent>
     <CardActions disableActionSpacing>
       <Update initialValues={{alias}} id={id} onSubmit={handleUpdateOutlet} />
